3b51b2b362056cc232031b890fd4208c7f0c720d
Luv <ks753159@gmail.com>
2026-06-10 16:24:42 +0000

    c1

---
+++ l1.js
@@ -0,0 +1,37 @@
+//objects
+//info:object ke form me store kara dete hai
+//0:name, 1: age,2:city:
+
+const user={
+    name:"Rohit",
+    age:20,
+    city:"kotdwar",
+    emailId:"negi@gmail.com",
+    amount:420
+}
+
+const users=[
+
+]
+function Display(food)
+{
+    console.log(food.name);
+    console.log(food.price);
+    console.log(food.rating);
+    console.log(food.rating);
+    console.log(food.imagelink);
+}
+
+const obj2={
+
+}
+const obj={
+    name:"Wednesdat chicken Bucket",
+    price:798,
+    rating:4.1,
+    description:"En"
+}
+
+for(const food in arr){
+    Display(food);
+}
