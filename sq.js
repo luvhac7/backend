0175d1c1926134571b347d7b332d691d753b593b
Luv <ks753159@gmail.com>
2026-06-11 22:48:45 +0530

    sq
    fix1

---
+++ sq.js
@@ -0,0 +1,24 @@
+const sq=num=>num*num;
+console.log(sq(8);
+greet();
+
+const user=()=>{
+  const obj= {
+      name:"Rohit",
+      age:20,
+  }
+}
+
+console.log(user());
+function hello(){
+  console.log("Hello world!");
+}();
+//call back function
+function greet(Callback){
+  console.log("hello");
+  Callback();
+}
+greet();
+meet(greet);
+
+
